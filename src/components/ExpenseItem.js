8bb857a571aaeb438e2343c298d986ab0affd67d
Björn Tirsén <bjorntirsen@gmail.com>
2021-07-20 09:50:56 +0200

    :lipstick: Add Styling

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,12 @@
+import './ExpenseItem.css'
+
 function ExpenseItem() {
   return (
-    <div>
-      <div>Date</div>
-      <div>
-        <h2>Title</h2>
-        <div>Amount</div>
+    <div className="expense-item">
+      <div>March 3, 2021</div>
+      <div className="expense-item__description">
+        <h2>Car Tyres</h2>
+        <div className="expense-item__price">$423.23</div>
       </div>
     </div>
   );
