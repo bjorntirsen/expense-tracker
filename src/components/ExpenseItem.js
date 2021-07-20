1f320222dfd252d003bd487803caa0835ff439e5
Björn Tirsén <bjorntirsen@gmail.com>
2021-07-20 10:29:54 +0200

    :sparkles: Dynamic props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,12 @@
-import './ExpenseItem.css'
+import './ExpenseItem.css';
 
-function ExpenseItem() {
+function ExpenseItem(props) {
   return (
-    <div className="expense-item">
-      <div>March 3, 2021</div>
-      <div className="expense-item__description">
-        <h2>Car Tyres</h2>
-        <div className="expense-item__price">$423.23</div>
+    <div className='expense-item'>
+      <div>{props.date.toISOString()}</div>
+      <div className='expense-item__description'>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
       </div>
     </div>
   );
